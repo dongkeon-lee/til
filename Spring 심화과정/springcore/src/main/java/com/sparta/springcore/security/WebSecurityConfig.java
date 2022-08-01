dacb81976c448f169bc3a226566a57e33a4243cb
dongkeon-lee <veha357@naver.com>
2022-08-01 09:43:45 +0900

    2-9

--- Spring 심화과정/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ Spring 심화과정/springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -5,11 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -35,15 +41,22 @@ public class WebSecurityConfig {
                         // 어떤 요청이든 '인증'
                         .anyRequest().authenticated()
                 )
-                // 로그인 기능 허용
+                // [로그인 기능]
                 .formLogin()
+                // 로그인 View 제공 (GET /user/login)
                 .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/user/login")
+                // 로그인 처리 후 성공 시 URL
                 .defaultSuccessUrl("/")
+                // 로그인 처리 후 실패 시 URL
                 .failureUrl("/user/login?error")
                 .permitAll()
                 .and()
-                //로그아웃 기능 허용
+                // [로그아웃 기능]
                 .logout()
+                // 로그아웃 처리 URL
+                .logoutUrl("/user/logout")
                 .permitAll();
 
         return http.build();
